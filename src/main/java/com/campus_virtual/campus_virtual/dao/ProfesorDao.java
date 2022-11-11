3d1f40a354bce168d47ee8d05fcbe95c4e7b97c3
m.navarro <m.navarro@MNAVARRO-L.aligntech.com>
2022-11-10 23:55:41 -0600

    Avance Michelle

--- src/main/java/com/campus_virtual/campus_virtual/dao/ProfesorDao.java
+++ src/main/java/com/campus_virtual/campus_virtual/dao/ProfesorDao.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.campus_virtual.campus_virtual.dao;
-
 import com.campus_virtual.campus_virtual.domain.Profesor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
